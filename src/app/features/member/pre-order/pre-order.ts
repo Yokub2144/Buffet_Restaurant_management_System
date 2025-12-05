a9d8b63e6c8cef2236315a80c653bd7f7baca250
Yokub2144 <Suwat.Paomong@gmail.com>
2025-12-05 23:04:33 +0700

    add folder

---
+++ src/app/features/member/pre-order/pre-order.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pre-order',
+  imports: [],
+  templateUrl: './pre-order.html',
+  styleUrl: './pre-order.scss',
+})
+export class PreOrder {
+
+}
