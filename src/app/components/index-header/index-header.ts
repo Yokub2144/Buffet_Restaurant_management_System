437f46305e27277b21c2208d78713706d057e0b9
aof20022002 <153268994+aof20022002@users.noreply.github.com>
2025-12-06 15:29:05 +0700

    test index

---
+++ src/app/components/index-header/index-header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-index-header',
+  imports: [],
+  templateUrl: './index-header.html',
+  styleUrl: './index-header.scss',
+})
+export class IndexHeader {
+
+}
