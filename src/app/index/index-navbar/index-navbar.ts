e59a7722d98eac9abffb2d5ca079b5b42ea37e1a
aof20022002 <153268994+aof20022002@users.noreply.github.com>
2025-12-06 16:47:15 +0700

    nav

---
+++ src/app/index/index-navbar/index-navbar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-index-navbar',
+  imports: [],
+  templateUrl: './index-navbar.html',
+  styleUrl: './index-navbar.scss',
+})
+export class IndexNavbar {
+
+}
--- src/app/index/index.ts
+++ src/app/index/index.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { IndexNavbar } from './index-navbar/index-navbar';
 
 @Component({
   selector: 'app-index',
-  imports: [],
+  imports: [IndexNavbar],
   templateUrl: './index.html',
   styleUrl: './index.scss',
 })
-export class Index {
-
-}
+export class Index {}
