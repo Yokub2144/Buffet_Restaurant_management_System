6d525d5d18e2936a7b92bc3cdea8a1e4ff7e6094
aof20022002 <153268994+aof20022002@users.noreply.github.com>
2025-12-06 18:14:31 +0700

    test index

--- src/app/index/index-navbar/index-navbar.ts
+++ src/app/index/index-navbar/index-navbar.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-index-navbar',
-  imports: [],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
   templateUrl: './index-navbar.html',
   styleUrl: './index-navbar.scss',
 })
-export class IndexNavbar {
-
-}
+export class IndexNavbar {}
