d8c73b3ac97b7c4bc11064b74776216e499aa745
Yokub2144 <Suwat.Paomong@gmail.com>
2025-12-06 19:21:50 +0700

    test

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { Dashboard } from './features/manager/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: Dashboard }];
